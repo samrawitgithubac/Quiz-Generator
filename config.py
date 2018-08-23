395011da70ab65d30584d16b66f847605b162656
Jay Toler <jay.toler@hotmail.com>
2018-08-23 13:45:29 -0700

    Added helper functions for the creation of the database. Added descriptions where they were needed.

--- config.py
+++ config.py
@@ -73,6 +73,91 @@ def updateUser(name, target, value, tableName='USERS', dbName='radioDB.db'):
     pass
 
 
+def readUserValue(name, target, tableName='USERS', dbName='radioDB.db'):
+    """
+       DEVELOPMENT
+       
+       takes a name and a target value as an input with the default values
+        for tableName and dbName
+       makes a query to the database to read only the specified value from
+        the users information
+       
+       INPUT STRING:
+        SELECT target FROM tableName WHERE Name=name
+              
+       input <string>
+       output <string> || <False>
+    """
+    pass
+
+
+def createTable(tableName, values, dbName='radioDB.db'):
+    """
+       DEVELOPMENT
+       
+       takes a table name and values with default name for dbName
+       makes a request to the database to create a table with the given
+        table name and associated values
+
+       NOTE: values needs to have the following format
+              (COLUMN_NAME DATA_TYPE)
+       
+       INPUT STRING:
+        CREATE TABLE tableName values
+       
+       input <string>
+       output <boolean>
+    """
+    pass
+
+
+def insertToDB(tableName, values, dbName='radioDB.db'):
+    """
+       DEVELOPMENT
+       
+       takes a table name and values with the default value for dbName
+       makes a request to the database to insert into the table with the given
+        table name the values provided
+       
+       INPUT STRING:
+        INSERT INTO tableName VALUES values
+       
+       input <string>
+       output <boolean>
+    """
+    pass
+
+
+def selectQuestion(groupID, subgroupID, questionNumber, dbName='radioDB.db'):
+    """
+       DEVELOPMENT
+       
+       takes a groupID, subgroupID, and questionNumber with default value for dbName
+       makes a request to the database to pull the specified question
+       NOTE: the output follows the following format
+              { QID : [ Question, Answer, [ Incorrect Answers ] ] }
+       
+       INPUT STRING:
+        SELECT * FROM QUESTIONS WHERE QID=QID
+       
+       input <string>
+       output <dictionary> || <False>
+    """
+    pass
+
+def selectRandomWithWeights(name, depreciator=0.2):
+    """
+       takes a name as input, with default value for depreciator
+       
+       first, pulls the full question Dictionary for the user
+       then, breaks 
+       
+       input <string>
+       output <list> || <False>
+    """
+    pass
+
+
 def colorRED(text):
     """
        returns the string of text in the color RED
--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 # -*- coding: utf-8 -*-
 
-def startUp():
+def startUp(dbName = 'radioDB.db'):
     """
-       MISSING DESCRIPTION
+       takes a dbname with default value
+       checks if a database already exists.
+       if no database is available, creates one.
+       populates the global variable 'users' for future use
        
-       input <None>
+       NOTE: The database has two tables (users, questions)
+               USERS:
+                uid:string name:string questionDict:string scores:string
+               QUESTIONS:
+                qid:string question:string answer:string jsonOptions:string
+       
+       input <string>
        output <boolean>
     """
     pass
@@ -12,7 +21,21 @@ def startUp():
 
 def parseQPoolandInsert(fName='TechQPool.txt', dbName='radioDB.db', tableName='QUESTIONS'):
     """
-       MISSING DESCRIPTION
+       takes default values for file name, dbName, and tableName
+       parses the question pool file and stores the result into the database
+       
+       NOTE: iterates over the entire file and collects
+              the following information
+              
+             [+] Question Section
+             [+] Question Subsection
+             [+] Question Number
+             [+] Question Posed
+             [+] Correct Answer
+             [+] All Possible Answers
+       
+       input <string>
+       output <boolean>
     """
     pass
 
@@ -24,6 +47,24 @@ def processUser():
        input <None>
        output <boolean>
     """
+    def invalidCharacters(test_name):
+        """
+           Takes test_name as input
+           checks if input has been given
+            if so, checks the value for the use of approprate characters only
+           returns boolean, True if all characters are valid
+        """
+        pass
+    
+    def validName(test_name):
+        """
+           Takes test_name as input
+           checks if input has been given
+            if so, check the value fot all test involving valid names
+           returns boolean, True if all check pass
+        """
+        pass
+
     pass
 
 
